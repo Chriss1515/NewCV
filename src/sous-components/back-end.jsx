7c8899e17a19359061b8fc6bbb4ae799630bc983
Chris <44951271+Chriss1515@users.noreply.github.com>
2022-06-14 20:28:50 +0200

    initial commit

---
+++ src/sous-components/back-end.jsx
@@ -0,0 +1,11 @@
+const BackEnd = () => {
+  return (
+    <>
+      <div className="front-content">
+        Content Back-end devras etre ici, vas-y code "HTML" "CSS" ...
+      </div>
+    </>
+  );
+};
+
+export default BackEnd;
---
+++ src/sous-components/font-end.jsx
@@ -0,0 +1,11 @@
+const FontEnd = () => {
+  return (
+    <>
+      <div className="front-content">
+        Content front-end devras etre ici, vas-y code "HTML" "CSS" ...
+      </div>
+    </>
+  );
+};
+
+export default FontEnd;
